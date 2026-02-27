0a0da78cb47987e0e32347c48ab539aae07e4e5a
Ventureé Official <officialventuree@gmail.com>
2026-02-27 14:30:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9832026749-c7bfb",
+  "appId": "1:345992008365:web:62a410a59bb0dc89392c24",
+  "apiKey": "AIzaSyCBeTiclt58GXqAFp366I1W_j3dqrrjhF8",
+  "authDomain": "studio-9832026749-c7bfb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "345992008365"
+};
